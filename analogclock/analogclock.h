5ed685fb7ff54941bf1f597b5ff8edb4bb410c41
voganesyan <oganesyan@cvisionlab.com>
2023-05-06 11:38:51 +0300

    code clean up

--- analogclock/analogclock.h
+++ analogclock/analogclock.h
@@ -1,12 +1,6 @@
-// Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
-
-#ifndef ANALOGCLOCK_H
-#define ANALOGCLOCK_H
-
+#pragma once
 #include <QWidget>
 
-//! [0]
 class AnalogClock : public QWidget
 {
     Q_OBJECT
@@ -17,6 +11,3 @@ public:
 protected:
     void paintEvent(QPaintEvent *event) override;
 };
-//! [0]
-
-#endif
