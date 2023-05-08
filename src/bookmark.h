40b60e62e888a331ddfd3ad4a895b02dd86b9275
voganesyan <oganesyan@cvisionlab.com>
2023-05-08 21:21:51 +0300

    minor update

--- src/bookmark.h
+++ src/bookmark.h
@@ -6,7 +6,7 @@ using namespace std::string_literals;
 struct Bookmark
 {
     explicit Bookmark(int id, long timestamp, long duration)
-        : name("Bookmark "s + std::to_string(id)), timestamp(timestamp), duration(duration) {};
+        : name("Bookmark "s + std::to_string(id)), timestamp(timestamp), duration(duration) {}
 
     long end_time() const { return timestamp + duration; }
 
